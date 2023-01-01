90b09d0b672a324257267128d6d0aca78e8f8de7
Heby T Paul <hebytpaul1111@gmail.com>
2023-01-02 05:03:28 +0530

    Added Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,12 @@
-import React from 'react'
 
-const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import Logo from '../assets/images/Logo-1.png';
 
-export default Footer
+const Footer = () => (
+  <Box mt="80px" bgcolor="#FFF3F4">
+    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Made with ❤️ by Heby Paul</Typography>
+  </Box>
+);
+
+export default Footer;
